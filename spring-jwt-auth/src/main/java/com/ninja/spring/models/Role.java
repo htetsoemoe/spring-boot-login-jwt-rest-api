64c416bfcbaa7f7c3ab0b0b3750c97a309ba09ee
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-08-30 11:53:02 +0630

    finished entities

---
+++ spring-jwt-auth/src/main/java/com/ninja/spring/models/Role.java
@@ -0,0 +1,47 @@
+package com.ninja.spring.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+	
+	public Role() {
+	}
+
+	public Role(ERole name) {
+		super();
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public ERole getName() {
+		return name;
+	}
+
+	public void setName(ERole name) {
+		this.name = name;
+	}
+}
